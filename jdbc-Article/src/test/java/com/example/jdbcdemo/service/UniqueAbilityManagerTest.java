98a9c208e8f02215dbd73764fac94477bfcfa893
student <julianna.mikucka@gmail.com>
2016-11-24 00:16:23 +0100

    zmiana koncepcji

--- jdbc-Article/src/test/java/com/example/jdbcdemo/service/UniqueAbilityManagerTest.java
+++ jdbc-Article/src/test/java/com/example/jdbcdemo/service/UniqueAbilityManagerTest.java
@@ -7,38 +7,46 @@ import java.util.List;
 import org.junit.Test;
 
 import com.example.jdbcdemo.domain.UniqueAbility;
+import com.example.jdbcdemo.domain.Article;
+
+import com.example.jdbcdemo.service.*;
 
 public class UniqueAbilityManagerTest {
 	
 	
-	UniqueAbilityManager uaM = new UniqueAbilityManager();
+	UniqueAbilityManager uaManager = new UniqueAbilityManager();
+	ArticleManager aManager = new ArticleManager();
 	
 	private final static String NAME_1 = "Tanczace Ostrze";
 	private final static String DESC_1 = "Szybkosc ataku zwiekszona o 20%";
 	
 	
-	@Test
-	public void checkConnection(){
-		assertNotNull(uaM.getConnection());
-	}
+//	@Test
+//	public void checkConnection(){
+//		assertNotNull(uaManager.getConnection());
+//	}
 	
-	@Test
-	public void checkAdding(){
-		
-		UniqueAbility ua = new UniqueAbility(NAME_1, DESC_1);
-		
-		uaM.clearUniqueAbilitiy();
-		assertEquals(1,uaM.addUniqueAbility(ua)); //sprawdza czy wykonalo sie query
-		
-	System.out.println("siemka");	
-	System.out.println(uaM.addUniqueAbility(ua));
-		
-		List<UniqueAbility> persons = uaM.getAllUniqueAbility();
-		UniqueAbility personRetrieved = persons.get(0);
-		
-		assertEquals(NAME_1, personRetrieved.getName());
-		assertEquals(DESC_1, personRetrieved.getDescription());
-		
-	}
+//	@Test
+//	public void checkAdding(){
+//		uaManager.clearUniqueAbilitiy();
+//		
+//		UniqueAbility ua = new UniqueAbility(NAME_1, DESC_1);
+//		
+//		uaManager.clearUniqueAbilitiy();
+//		aManager.clearArticles();
+//		
+//
+//		assertEquals(1,uaManager.addUniqueAbility(ua)); //sprawdza czy wykonalo sie query
+//		
+//	System.out.println("siemka");	
+//	System.out.println(uaManager.addUniqueAbility(ua));
+//		
+//		List<UniqueAbility> uniAbilities = uaManager.getAllUniqueAbility();
+//		UniqueAbility personRetrieved = uniAbilities.get(0);
+//		
+//		assertEquals(NAME_1, personRetrieved.getName());
+//		assertEquals(DESC_1, personRetrieved.getDescription());
+//		
+//	}
 
 }
