4515ad819701b3d26ebfe21a5b69a95afcded219
student <julianna.mikucka@gmail.com>
2016-11-24 06:41:54 +0100

    4 test - ArticleGet - dziala

--- jdbc-Article/src/test/java/com/example/jdbcdemo/service/ArticleManagerTest.java
+++ jdbc-Article/src/test/java/com/example/jdbcdemo/service/ArticleManagerTest.java
@@ -10,94 +10,163 @@ import org.junit.Test;
 import com.example.jdbcdemo.domain.Article;
 import com.example.jdbcdemo.domain.UniqueAbility;
 
-//import com.example.jdbcdemo.service.UniqueAbilityManager;
 
 public class ArticleManagerTest {
-
+	
+	
 	ArticleManager articleManager = new ArticleManager();
-
-	private final static String NAME_1 = "Ostrze nieskonczonosci";
-	private final static int DMG_1 = 420;
-	private final static String TYPE_1 = "Miecz";
-	 
-	private final static String NAME_2 = "Szybki jak wiatr2"; 
-	private final static String DESC_2 = "Predkosc ruchu zwiekszona o 20%";
-
-	@Before
+	
+	private final static String MODEL_1 = "D100";
+	private final static int SHOTS_1 = 80000;
+	private final static double PRICE_1 = 200;
+	private final static boolean SOLD_1 = false;
+	
+	private final static String NAME_1 = "bag";
+	private final static double PRICE_2 = 69;
+	
+	private final static String NEWMODEL_1 = "D200";
+	private final static int NEWSHOTS_1 = 75000;
+	private final static double NEWPRICE_1 = 450;
+	private final static boolean NEWSOLD_1 = true;
+	
+	@Before 
 	public void initialize() {
-		UniqueAbility ua = new UniqueAbility();
-		ua.setName("Szybki jak wiatr");
-		ua.setDescription("Predkosc ruchu zwiekszona o 20%");
+		UniqueAbility abi = new UniqueAbility();
+		abi.setDesc(MODEL_1);
+		abi.setPower(PRICE_1);
+		abi.setMagic(SOLD_1);
+		abi.setLevel(SHOTS_1);
 		
+		articleManager.clearUniqueAbility();
+		articleManager.clearArticle();
 		
-		articleManager.clearUniqueAbilitiy();
-		articleManager.clearArticles();
-		
-
-		articleManager.addUniqueAbility(ua);
-	}	
+		articleManager.addUniqueAbility(abi);
+	}
 	
 	@Test
-	public void checkConnection() {
+	public void checkConnection(){
 		assertNotNull(articleManager.getConnection());
 	}
 	
 	@Test
-	public void checkAddingUA(){
-		articleManager.clearUniqueAbilitiy();
-		
+	public void checkAdding(){
+		articleManager.clearUniqueAbility();
 		UniqueAbility ua = new UniqueAbility();
-		ua.setName(NAME_2);
-		ua.setDescription(DESC_2);
+		ua.setDesc(MODEL_1);
+		ua.setPower(PRICE_1);
+		ua.setMagic(SOLD_1);
+		ua.setLevel(SHOTS_1);
 		
-		articleManager.clearArticles();
-		articleManager.clearUniqueAbilitiy();
+		articleManager.clearUniqueAbility();
+		articleManager.clearArticle();
 		
 		assertEquals(1,articleManager.addUniqueAbility(ua));
 		List<UniqueAbility> uas = articleManager.getAllUniqueAbility();
-		UniqueAbility uaRetrived = uas.get(0);
+		UniqueAbility uasRetrieved = uas.get(0);
 		
-		assertEquals(NAME_2,uaRetrived.getName());
-		assertEquals(DESC_2,uaRetrived.getDescription());
+		assertEquals(MODEL_1, uasRetrieved.getDesc());
+		assertEquals(PRICE_1, uasRetrieved.getPower(), 0.1);
+		assertEquals(SOLD_1, uasRetrieved.getMagic());
+		assertEquals(SHOTS_1, uasRetrieved.getLevel());
+
 	}
+	
+	@Test
+	public void checkAddingSecond() {		
+		List<UniqueAbility> unis = articleManager.getAllUniqueAbility();
+		UniqueAbility abiRetrieved = unis.get(0);
+		
+		Article article = new Article();
+		article.setName(NAME_1);
+		article.setDmg(PRICE_2);
+		article.setUaId(abiRetrieved.getId());
+		articleManager.addArticle(abiRetrieved, article);
+		
+		Article artRetrived = abiRetrieved.getArticle().get(0);
+//		System.out.println("t");
+//		System.out.println(artRetrived.getName());
+//		System.out.println(artRetrived.getDmg());
+		
+		assertEquals(NAME_1, artRetrived.getName());
+		assertEquals(PRICE_2, artRetrived.getDmg(), 0.1);
+	}
+	
+	@Test
+	public void AccessoryGet() {
 
-
-
-	// @Test
-	// public void sprawdzenie() {
-	// UniqueAbility ua = new UniqueAbility("Niezlomnyqe",
-	// "Obrazenia krytyczne zwiekszone o 20%qwe");
-	// assertEquals(1, articleManager.addUniqueAbility(ua));
-	//
-	// System.out.println("Czy pokaze id?2");
-	// System.out.println(articleManager.select_id_from_uniqueAbility("Niezlomnyqe"));
-	//
-	// }
-
-	// @Test
-	// public void checkAdding() {
-	// UniqueAbility ua = new UniqueAbility("Niezlomny",
-	// "Obrazenia krytyczne zwiekszone o 20%");
-	// assertEquals(1, uaM.addUniqueAbility(ua));
-	//
-	// System.out.println("Czy pokaze id?");
-	// System.out.println(uaM.select_id_from_uniqueAbility("Niezlomny"));
-	//
-	// Article article = new Article(NAME_1, DMG_1, TYPE_1,
-	// uaM.select_id_from_uniqueAbility("Niezlomny"));
-	// //
-	// // articleManager.clearArticles();
-	// System.out.println("dodalo sie?");
-	//
-	// assertEquals(1, articleManager.addArticle(article));
-	// System.out.println(articleManager.addArticle(article));
-	//
-	// List<Article> articles = articleManager.getAllArticle();
-	// Article articleRetrieved = articles.get(0);
-	//
-	// assertEquals(NAME_1, articleRetrieved.getName());
-	// assertEquals(DMG_1, articleRetrieved.getDmg());
-	//
-	// }
-
+		List<UniqueAbility> unis = articleManager.getAllUniqueAbility();
+		UniqueAbility abiRetrieved = unis.get(0);
+		
+		Article article = new Article();
+		article.setName(NAME_1);
+		article.setDmg(PRICE_2);
+		article.setUaId(abiRetrieved.getId());
+		articleManager.addArticle(abiRetrieved, article);
+		
+		unis = articleManager.getAllUniqueAbility();
+		abiRetrieved = unis.get(0);
+		
+		List<Article> art = articleManager.getAllArticle(abiRetrieved.getId());
+		Article artRetrived = art.get(0);
+		assertEquals(1, art.size());
+		assertEquals(NAME_1, artRetrived.getName());
+		assertEquals(PRICE_2, artRetrived.getDmg(), 0.1);
+	}
+	
+//	@Test
+//	public void checkEditMain() {
+//		List<Camera> cameras = cameraManager.getAllCameras();
+//		Camera camerasRetrieved = cameras.get(0);
+//		camerasRetrieved.setModel(NEWMODEL_1);
+//		camerasRetrieved.setPrice(NEWPRICE_1);
+//		camerasRetrieved.setShots(NEWSHOTS_1);
+//		camerasRetrieved.setSold(NEWSOLD_1);
+//		cameraManager.editCamera(camerasRetrieved.getId(), camerasRetrieved);
+//		
+//		cameras = cameraManager.getAllCameras();
+//		Camera edited = cameras.get(0);
+//		assertEquals(NEWMODEL_1, edited.getModel());
+//		assertEquals(NEWPRICE_1, edited.getPrice(), 0.01);
+//		assertEquals(NEWSHOTS_1, edited.getShots());
+//		assertEquals(NEWSOLD_1, edited.getSold());
+//	}
+//
+//	@Test
+//	public void CameraDelete(){
+//
+//		Camera camera = new Camera();
+//		camera.setModel(NEWMODEL_1);
+//		camera.setPrice(NEWPRICE_1);
+//		camera.setSold(NEWSOLD_1);
+//		camera.setShots(NEWSHOTS_1);
+//		
+//		cameraManager.addCamera(camera);
+//		
+//		List<Camera> cameras = cameraManager.getAllCameras();
+//		Camera camerasRetrieved = cameras.get(0);
+//		
+//		assertEquals(2, cameras.size());
+//		
+//		Accessory accessory = new Accessory();
+//		accessory.setName(NAME_1);
+//		accessory.setPrice(PRICE_2);
+//		accessory.setOwnerId(camerasRetrieved.getId());
+//		cameraManager.addAccessory(camerasRetrieved, accessory);
+//
+//		cameras = cameraManager.getAllCameras();
+//		camerasRetrieved = cameras.get(0);
+//
+//	    // deleting the user
+//	    cameraManager.deleteCamera(camerasRetrieved);
+//	    cameras = cameraManager.getAllCameras();
+//	    
+//	    // checking deletion
+//	    assertEquals(1, cameras.size());
+//	    
+//	    Camera otherCamera = cameras.get(0);
+//		assertEquals(NEWMODEL_1, otherCamera.getModel());
+//		assertEquals(NEWPRICE_1, otherCamera.getPrice(), 0.1);
+//		assertEquals(NEWSOLD_1, otherCamera.getSold());
+//		assertEquals(NEWSHOTS_1, otherCamera.getShots());
+//	}
 }
